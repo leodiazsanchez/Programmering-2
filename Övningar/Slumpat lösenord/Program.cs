2c5d3556be68569e03542536cb49d8776cebebfe
Leo2659 <ledia_01@edu.sollentuna.se>
2020-03-31 10:18:53 +0200

    2020-03-31

--- Övningar/Slumpat lösenord/Program.cs
+++ Övningar/Slumpat lösenord/Program.cs
@@ -25,7 +25,7 @@ namespace Slumpat_lösenord
                     char siffra = Convert.ToChar(random.Next(48, 58));
                     lösenord += siffra;
                 }
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < 3; i++)
                 {
                     char versal = Convert.ToChar(random.Next(65, 91));
                     lösenord += versal;
@@ -37,7 +37,7 @@ namespace Slumpat_lösenord
             {
                 // Create an instance of StreamReader to read from a file.
                 // The using statement also closes the StreamReader.
-                using (StreamWriter sw = new StreamWriter("Slump.txt"))
+                using (StreamWriter sw = new StreamWriter("Slump.txt", true))
                 {
                     sw.WriteLine(user + "\t" + Lösen());
                 }
@@ -57,6 +57,7 @@ namespace Slumpat_lösenord
                     string line;
                     // Read and display lines from the file until the end of 
                     // the file is reached.
+                    Console.WriteLine("User" + "\t" + "Password");
                     while ((line = sr.ReadLine()) != null)
                     {
                         Console.WriteLine(line);
---
+++ Övningar/Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                StreamWriter sw = new StreamWriter("total.txt");
+                using (StreamReader sr = new StreamReader("resultat.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(line);
+                        string[] words = line.Split(" ");
+                        string str = words[0];
+                        double medelvärde = (double.Parse(words[1]) + double.Parse(words[2]))/2;
+                        double total = double.Parse(words[1]) + double.Parse(words[2]);
+                        sw.WriteLine(words[0] + "\t" + total);
+                        str += " Medelvärde: " + medelvärde.ToString() + " Total: " + total.ToString();
+                        Console.WriteLine(str);
+                    }
+                    sw.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
